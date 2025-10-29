cb8f1f69f2eb087130603314003d34b64ee54b9d
determaer <determaer2012@gmail.com>
2025-10-29 19:39:48 +0700

    chore: deps up; fix: update useEditNode with generic

--- src/modals/useEditNode.ts
+++ src/modals/useEditNode.ts
@@ -1,17 +1,27 @@
-import { Modal, useModal } from '@determaer/vue-use-modal'
-import { type Label, instanceOfLabel } from '@determaer/cmkd';
+import { Modal, useModal } from "@determaer/vue-use-modal";
+import { type Label, instanceOfLabel } from "@determaer/cmkd";
 
-export const useModalEditNode = async () : Promise<Label | never> => {
-  const data = await useModal({
-    //в component должен быть встроен emit change(key: value)
-    component: import('./EditNode.vue'),
-    Modal,
-  })
-  if (data.label){
-    const label = data.label
-    if (instanceOfLabel(label)){
-      return label
-    }
-  }
-  throw new Error()
+type ReturnEditNode = {
+  label: Label;
+};
+
+function typeofReturnEditNode(value: unknown): value is ReturnEditNode {
+  return (
+    value instanceof Object && "label" in value && instanceOfLabel(value.label)
+  );
 }
+
+export const useModalEditNode = async (): Promise<Label | never> => {
+  try {
+    const data = await useModal<ReturnEditNode>({
+      //в component должен быть встроен emit change(key: value)
+      component: import("./EditNode.vue"),
+      Modal,
+    });
+    if (typeofReturnEditNode(data)) {
+      const label = data.label;
+      return label;
+    }
+  } catch {}
+  throw new Error();
+};
